1961e1d48c94e87dfd948a5d2420a8e3440e1a93
Tyler Horvat <kibur1986@gmail.com>
2017-06-13 01:46:06 -0700

    PUshing up pokerhand. Final try to get all files uploaded. Please grade this submission

---
+++ src/model/Suit.java
@@ -0,0 +1,14 @@
+/* CSC 335 Summer 2017 Project 1
+ * Tyler Horvat
+ * 
+ * This enum holds all possible values for suits
+ * in a deck of cards
+ * 
+ */
+
+package model;
+
+public enum Suit {
+  CLUBS, DIAMONDS, HEARTS, SPADES;
+}
+  
---
+++ src/tests/SuitTest.java
@@ -0,0 +1,46 @@
+/* CSC 335 Summer 2017 Project 1
+ * Tyler Horvat
+ * 
+ * This class runs tests on enum Suit
+ */
+
+package tests;
+
+/**
+ * A unit test for enum Suit
+ * 
+ * @author Rick Mercer
+ */
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import model.Suit;
+
+public class SuitTest {
+  
+  @Test
+  public void testSuitNamesAreInOfTypeSuit() {
+    // Kind of silly, but there is not much to test in enum Suit
+    assertTrue(Suit.CLUBS instanceof Suit);
+    assertTrue(Suit.DIAMONDS instanceof Suit);
+    assertTrue(Suit.HEARTS instanceof Suit);
+    assertTrue(Suit.SPADES instanceof Suit);
+  }
+
+  @Test
+  public void testSuit() {
+    // Onyl for code coverage
+    assertEquals(Suit.CLUBS, Suit.CLUBS);
+    assertEquals(Suit.DIAMONDS, Suit.DIAMONDS);
+    assertEquals(Suit.HEARTS, Suit.HEARTS);
+    assertEquals(Suit.SPADES, Suit.SPADES);
+  }
+  
+  @Test
+  public void testSuitWithTheBuiltInMethodValues() {
+    String result = "";
+    for (Suit suit : Suit.values())
+      result += suit + "_";
+    assertEquals("CLUBS_DIAMONDS_HEARTS_SPADES_", result);
+  }
+}
